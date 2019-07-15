dfe04acb2ba6478b7961edab51e889d610758bdc
Domenica Cañizares <Domenica Caizares@Domenica>
2019-07-14 22:45:22 -0500

    Generar Graficas

--- src/ec/edu/ups/vista/ReportesPDF.java
+++ src/ec/edu/ups/vista/ReportesPDF.java
@@ -5,6 +5,19 @@
  */
 package ec.edu.ups.vista;
 
+import ec.edu.ups.controlador.BaseDatos;
+import ec.edu.ups.controlador.ContorladorBasePersona;
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.util.Set;
+import org.jfree.chart.ChartFactory;
+import org.jfree.chart.ChartPanel;
+import org.jfree.chart.JFreeChart;
+import org.jfree.chart.plot.CategoryPlot;
+import org.jfree.chart.plot.PlotOrientation;
+import org.jfree.data.category.DefaultCategoryDataset;
+import org.jfree.data.general.DefaultPieDataset;
+
 /**
  *
  * @author Domenica Cañizares
@@ -14,8 +27,52 @@ public class ReportesPDF extends javax.swing.JFrame {
     /**
      * Creates new form ReportesPDF
      */
+    int cont = 0;
+    int cont1 = 0;
+    int cont2 = 0;
+    int cont3 = 0;
+    int cont4 = 0;
+    int conp1 = 0;
+    int conp2 = 0;
+    int conp3 = 0;
+    int conp4 = 0;
+    int conp5 = 0;
+    int conp6 = 0;
+    int conp7 = 0;
+    int conp8 = 0;
+    Double suma1 = 0.00;
+    Double suma2 = 0.00;
+    Double suma3 = 0.00;
+    Double suma4 = 0.00;
+    Double suma5 = 0.00;
+    Double suma6 = 0.00;
+    Double suma7 = 0.00;
+    Double suma8 = 0.00;
+    double[] sumas;
+    int[] contadores;
+    double[] acumulador;
+    String url = "jdbc:postgresql://localhost:5432/BaseDatosReportes";
+    String user = "postgres";
+    String password = "johysydanny15";
+    Set<Persona> listaPersonas;
+
     public ReportesPDF() {
         initComponents();
+
+        //ContorladorBasePersona controlador = new ContorladorPersona(url, user, password);
+        BaseDatos base = new BaseDatos(url, user, password);
+        base.conectar();
+        ContorladorBasePersona controlador = new ContorladorBasePersona();
+
+        sumas = new double[38];
+        contadores = new int[38];
+        acumulador = new double[38];
+        listaPersonas = controlador.printPer();
+
+    }
+
+    public void generarGraficas() {
+        condiciones();
     }
 
     /**
@@ -27,21 +84,261 @@ public class ReportesPDF extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
+        jButton1 = new javax.swing.JButton();
+        jButton2 = new javax.swing.JButton();
+        jPanelG2 = new javax.swing.JPanel();
+        jPanelG1 = new javax.swing.JPanel();
+        jPanelG3 = new javax.swing.JPanel();
+
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+
+        jButton1.setText("Generar PDF");
+
+        jButton2.setText("Generar Graficos");
+        jButton2.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton2ActionPerformed(evt);
+            }
+        });
+
+        jPanelG2.setBorder(javax.swing.BorderFactory.createTitledBorder("Grafico 2"));
+
+        javax.swing.GroupLayout jPanelG2Layout = new javax.swing.GroupLayout(jPanelG2);
+        jPanelG2.setLayout(jPanelG2Layout);
+        jPanelG2Layout.setHorizontalGroup(
+            jPanelG2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 339, Short.MAX_VALUE)
+        );
+        jPanelG2Layout.setVerticalGroup(
+            jPanelG2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 286, Short.MAX_VALUE)
+        );
+
+        jPanelG1.setBorder(javax.swing.BorderFactory.createTitledBorder("Grafico 1"));
+
+        javax.swing.GroupLayout jPanelG1Layout = new javax.swing.GroupLayout(jPanelG1);
+        jPanelG1.setLayout(jPanelG1Layout);
+        jPanelG1Layout.setHorizontalGroup(
+            jPanelG1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 339, Short.MAX_VALUE)
+        );
+        jPanelG1Layout.setVerticalGroup(
+            jPanelG1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 286, Short.MAX_VALUE)
+        );
+
+        jPanelG3.setBorder(javax.swing.BorderFactory.createTitledBorder("Grafico 3"));
+
+        javax.swing.GroupLayout jPanelG3Layout = new javax.swing.GroupLayout(jPanelG3);
+        jPanelG3.setLayout(jPanelG3Layout);
+        jPanelG3Layout.setHorizontalGroup(
+            jPanelG3Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 339, Short.MAX_VALUE)
+        );
+        jPanelG3Layout.setVerticalGroup(
+            jPanelG3Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 286, Short.MAX_VALUE)
+        );
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 400, Short.MAX_VALUE)
+            .addGroup(layout.createSequentialGroup()
+                .addGap(226, 226, 226)
+                .addComponent(jPanelG3, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap(215, Short.MAX_VALUE))
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                .addGap(0, 0, Short.MAX_VALUE)
+                .addComponent(jPanelG2, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(30, 30, 30))
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addComponent(jButton1)
+                .addGap(18, 18, 18)
+                .addComponent(jButton2)
+                .addGap(271, 271, 271))
+            .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                .addGroup(layout.createSequentialGroup()
+                    .addGap(20, 20, 20)
+                    .addComponent(jPanelG1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addContainerGap(421, Short.MAX_VALUE)))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 300, Short.MAX_VALUE)
+            .addGroup(layout.createSequentialGroup()
+                .addContainerGap()
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jButton1)
+                    .addComponent(jButton2))
+                .addGap(18, 18, 18)
+                .addComponent(jPanelG2, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(18, 18, 18)
+                .addComponent(jPanelG3, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap(57, Short.MAX_VALUE))
+            .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                .addGroup(layout.createSequentialGroup()
+                    .addGap(55, 55, 55)
+                    .addComponent(jPanelG1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addContainerGap(381, Short.MAX_VALUE)))
         );
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
+
+    private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
+
+        generarGraficas();
+
+    }//GEN-LAST:event_jButton2ActionPerformed
+    public void condiciones() {
+
+        for (Persona persona : listaPersonas) {
+            if (persona.getEdad() >= 16 && persona.getEdad() <= 20) {
+                // System.out.println(persona.toString());
+                if (persona.getGenero().equals("Masculino")) {
+                    conp1++;
+                    suma1 = suma1 + persona.getSueldo();
+                }
+                if (persona.getGenero().equals("Femenino")) {
+                    conp2++;
+                    suma2 = suma2 + persona.getSueldo();
+                }
+                cont1++;
+            }
+
+            if (persona.getEdad() >= 21 && persona.getEdad() <= 30) {
+                //System.out.println(persona.toString());
+
+                if (persona.getGenero().equals("Masculino")) {
+                    conp3++;
+                    suma3 = suma3 + persona.getSueldo();
+                }
+                if (persona.getGenero().equals("Femenino")) {
+                    conp4++;
+                    suma4 = suma4 + persona.getSueldo();
+                }
+                cont2++;
+            }
+
+            if (persona.getEdad() >= 31 && persona.getEdad() <= 40) {
+                //System.out.println(persona.toString());
+                if (persona.getGenero().equals("Masculino")) {
+                    conp5++;
+                    suma5 = suma5 + persona.getSueldo();
+                }
+                if (persona.getGenero().equals("Femenino")) {
+                    conp6++;
+                    suma6 = suma6 + persona.getSueldo();
+                }
+                cont3++;
+            }
+
+            if (persona.getEdad() >= 41) {
+                //System.out.println(persona.toString());
+                if (persona.getGenero().equals("Masculino")) {
+                    conp7++;
+                    suma7 = suma7 + persona.getSueldo();
+                }
+                if (persona.getGenero().equals("Femenino")) {
+                    conp8++;
+                    suma8 = suma8 + persona.getSueldo();
+                }
+                cont4++;
+            }
+            cont++;
+        }
+
+        int i = 0;
+        for (int ed = 16; ed <= 53; ed++) {
+
+            for (Persona persona : listaPersonas) {
+                if (persona.getEdad() == ed) {
+                    sumas[i] = sumas[i] + persona.getSueldo();
+                    contadores[i]++;
+                }
+            }
+            if (contadores[i] == 0) {
+                acumulador[i] = 0;
+            } else {
+                acumulador[i] = sumas[i] / contadores[i];
+            }
+            i++;
+        }
+        /*grafico1();
+         grafico2();
+         grafico3();*/
+    }
+
+    public void grafico1() {
+        DefaultPieDataset data = new DefaultPieDataset();
+        data.setValue("Entre 16-20", cont1);
+        data.setValue("Entre 21-30", cont2);
+        data.setValue("Entre 31-40", cont3);
+        data.setValue("Mayores a 40", cont4);
+
+        // Creando el Grafico
+        JFreeChart chart = ChartFactory.createPieChart(
+                "Promedio por Edades",
+                data,
+                true,
+                true,
+                false);
+
+        // Mostrar Grafico
+        ChartPanel frame = new ChartPanel(chart);
+        jPanelG1.setLayout(new java.awt.BorderLayout());
+        jPanelG1.add(frame, BorderLayout.CENTER);
+        jPanelG1.validate();
+
+    }
+
+    public void grafico2() {
+        // Fuente de Datos
+        DefaultCategoryDataset dataset = new DefaultCategoryDataset();
+        dataset.setValue((suma2 / conp2), "Mujeres", "16-20");
+        dataset.setValue((suma1 / conp1), "Hombres", "16-20");
+        dataset.setValue((suma4 / conp4), "Mujeres", "21-30");
+        dataset.setValue((suma3 / conp3), "Hombres", "21-30");
+        dataset.setValue((suma6 / conp6), "Mujeres", "31-40");
+        dataset.setValue((suma5 / conp5), "Hombres", "31-40");
+        dataset.setValue(0, "Mujeres", "Mayores 40");
+        dataset.setValue((suma7 / conp7), "Hombres", "Mayores 40");
+
+        // Creando el Grafico
+        JFreeChart chart = ChartFactory.createBarChart3D("Promedio Salario por Genero", "Genero", "Salario",
+                dataset, PlotOrientation.VERTICAL, true, true, false);
+        chart.getTitle().setPaint(Color.black);
+        CategoryPlot p = chart.getCategoryPlot();
+        p.setRangeGridlinePaint(Color.red);
+        // Mostrar Grafico
+        ChartPanel chartPanel = new ChartPanel(chart);
+        jPanelG2.setLayout(new java.awt.BorderLayout());
+        jPanelG2.add(chartPanel, BorderLayout.CENTER);
+        jPanelG2.validate();
+    }
+
+    public void grafico3() {
+        DefaultCategoryDataset line_chart_dataset = new DefaultCategoryDataset();
+
+        int ed = 16;
+        for (int i = 0; i < sumas.length; i++) {
+            String edad = String.valueOf(ed);
+            line_chart_dataset.addValue(acumulador[i], "salario", edad);
+            ed++;
+        }
+
+        // Creando el Grafico
+        JFreeChart chart = ChartFactory.createLineChart("Promedio por Edad",
+                "Edades", "Salario", line_chart_dataset, PlotOrientation.VERTICAL,
+                true, true, false);
+
+        // Mostrar Grafico
+        ChartPanel chartPanel1 = new ChartPanel(chart);
+        jPanelG3.setLayout(new java.awt.BorderLayout());
+        jPanelG3.add(chartPanel1, BorderLayout.CENTER);
+        jPanelG3.validate();
+    }
 
     /**
      * @param args the command line arguments
@@ -57,16 +354,21 @@ public class ReportesPDF extends javax.swing.JFrame {
                 if ("Nimbus".equals(info.getName())) {
                     javax.swing.UIManager.setLookAndFeel(info.getClassName());
                     break;
+
                 }
             }
         } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(ReportesPDF.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(ReportesPDF.class
+                    .getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(ReportesPDF.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(ReportesPDF.class
+                    .getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(ReportesPDF.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(ReportesPDF.class
+                    .getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(ReportesPDF.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(ReportesPDF.class
+                    .getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
         //</editor-fold>
 
@@ -79,5 +381,17 @@ public class ReportesPDF extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton jButton1;
+    private javax.swing.JButton jButton2;
+    private javax.swing.JPanel jPanelG1;
+    private javax.swing.JPanel jPanelG2;
+    private javax.swing.JPanel jPanelG3;
+    private javax.swing.JPanel panelG3;
+    private javax.swing.JPanel panelG4;
+    private javax.swing.JPanel panelG5;
+    private javax.swing.JPanel panelG6;
+    private javax.swing.JPanel panelG7;
+    private javax.swing.JPanel panelG8;
+    private javax.swing.JPanel panelG9;
     // End of variables declaration//GEN-END:variables
 }
